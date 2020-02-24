5df24bd712b78fd8b1b69c11d87cbf453352fa85
Grambulf <1233515+ramshazar@users.noreply.github.com>
2020-02-24 16:44:49 +0100

    initial commit

---
+++ evil-dockerfile/my_wrapper-script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Everything is fine, I am running now for 300 seconds."
+sleep 300
---
+++ malicious-nginx/static-files/installer.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Installing everything needed by our application"
+sleep 2
+echo "Done"
